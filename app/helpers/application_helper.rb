9a2712ec67374eec11afcaa3642f4b1510ca2061
Simon <acarigua88@mail.com>
2020-09-20 17:27:05 -0300

    authentication-one

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def logged?
+        session[:user_id].present?
+    end
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227134331) do
+ActiveRecord::Schema.define(version: 20200920201455) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -18,6 +21,41 @@ ActiveRecord::Schema.define(version: 20190227134331) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "users_id"
+    t.index ["user_id"], name: "index_stories_on_user_id"
+    t.index ["users_id"], name: "index_stories_on_users_id"
   end
 
+  create_table "user", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "usernames", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "stories", "users"
+  add_foreign_key "stories", "users", column: "users_id"
 end
